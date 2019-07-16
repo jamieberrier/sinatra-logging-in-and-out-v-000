f07c19ec4e2e3bed01863b203fb1305e1756089d
jamieberrier <berrier.jamie@gmail.com>
2019-07-16 17:13:50 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
   def self.current_user(session)
     binding.pry
-    @user = User.find_by(user_id: session[:user_id])
+    @user = User.find_by(id: session[:user_id])
   end
 
   def self.is_logged_in?
